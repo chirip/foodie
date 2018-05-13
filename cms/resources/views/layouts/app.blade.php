610d88bfcc4d967d7e7a281251b8ee18c616d24b
gs_hashimoto <aoiasagao.s@gmail.com>
2018-05-13 09:33:03 +0000

    jquery3.3.1 add js

--- cms/resources/views/layouts/app.blade.php
+++ cms/resources/views/layouts/app.blade.php
@@ -13,12 +13,8 @@
 
     <!-- Styles -->
 
-    <!--ajaxでのエラー回避にためjqueryの src httpsは削除-->
-    <script
-      src="//code.jquery.com/jquery-3.3.1.js"
-      integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-      crossorigin="anonymous">
-    </script>
+    <script src="{{ asset('/js/jquery-3.3.1.min.js') }}"></script>
+
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 

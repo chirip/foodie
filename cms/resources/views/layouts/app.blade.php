b11d9b60b7a2ae9db4fd82b08703ba6cc8d2afae
gs_hashimoto <aoiasagao.s@gmail.com>
2018-05-14 05:10:59 +0000

    rewrite bootstrap cdn

--- cms/resources/views/layouts/app.blade.php
+++ cms/resources/views/layouts/app.blade.php
@@ -13,22 +13,14 @@
 
     <!-- Styles -->
 
-    <!--ajaxでのエラー回避にためjqueryの src httpsは削除-->
-    <script
-      src="//code.jquery.com/jquery-3.3.1.js"
-      integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-      crossorigin="anonymous">
-    </script>
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-    <!--api sampleに記載あり-->
-    <link href="css/search.css" rel="stylesheet" type="text/css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    
+     <!--api sampleに記載あり-->
+    <link href="css/map.css" rel="stylesheet" type="text/css">
 
 
 
@@ -56,18 +48,17 @@
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
-              <ul class="nav navbar-nav">
-                <li class="{{ (Request::is('/') ? 'active' : '') }}">
-                    <a href="{{ url('') }}"><i class="fa fa-home"></i> Home</a>
-                </li>
-                <li class="{{ (Request::is('favorites') ? 'active' : '') }}">
-                    <a href="{{ url('favorites') }}">Favorites</a>
-                </li>
-                <li class="{{ (Request::is('mainpage') ? 'active' : '') }}">
-                    <a href="{{ url('mainpage') }}">Mainpage</a>
-                </li>
-
-            </ul>
+                     <ul class="nav navbar-nav">
+                        <li class="{{ (Request::is('/') ? 'active' : '') }}">
+                            <a href="{{ url('') }}"><i class="fa fa-home"></i> Search</a>
+                        </li>
+                        <li class="{{ (Request::is('favorites') ? 'active' : '') }}">
+                            <a href="{{ url('favorites') }}">Favorites</a>
+                        </li>
+                        <li class="{{ (Request::is('mainpage') ? 'active' : '') }}">
+                            <a href="{{ url('mainpage') }}">Mainpage</a>
+                        </li>
+                    </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
@@ -105,6 +96,15 @@
         @yield('content')
 
     <!-- Scripts -->
+        <!--ajaxでのエラー回避にためjqueryの src httpsは削除-->
+    <script
+      src="//code.jquery.com/jquery-3.3.1.js"
+      integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+      crossorigin="anonymous">
+       <!-- Latest compiled and minified JavaScript -->
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    </script>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>

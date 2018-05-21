2d584b7a1e24cf1cc1fb928bc45fbc4151c010ba
gs_hashimoto <aoiasagao.s@gmail.com>
2018-05-21 05:53:38 +0000

    change log out icon

--- cms/resources/views/layouts/app.blade.php
+++ cms/resources/views/layouts/app.blade.php
@@ -75,7 +75,11 @@
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->name }} 
+                                    <img class="user_icon" src="{{ asset('/image/icon-face.png') }}" alt="test_phote">
+
+                                    <!--<span class="caret"></span> <!--▼マーク-->
+
                                 </a>
 
                                 <ul class="dropdown-menu">
